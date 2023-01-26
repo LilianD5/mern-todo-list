ea3b7fdcc2eb7d69acb3183dc29f8b779dc5e8c5
LilianD5 <diazliliandev@gmail.com>
2023-01-26 22:24:01 +0100

    Add : Schema, API Endpoints

--- api/server.js
+++ api/server.js
@@ -15,4 +15,45 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo-list")
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+// Endpoints
+const Task = require("./models/Tasks");
+
+// Get all tasks
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find();
+
+    res.json(tasks);
+})
+
+// New task
+app.post("/task/new", (req, res) => {
+    const task = new Task({
+        description: req.body.description
+    });
+
+    task.save();
+
+    res.json(task);
+})
+
+// Delete task
+app.delete("/task/delete/:id", async (req, res) => {
+    const result = await Task.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
+
+// Toggle complete state
+app.put("/task/complete/:id", async (req, res) => {
+    const task = await Task.findById(req.params.id);
+
+    task.complete = !task.complete;
+
+    task.save();
+
+    res.json(task);
+})
+
+// Server listening on port XXXX
+const port = 3001
+app.listen(port, () => console.log(`Server started on port ${port}`));
