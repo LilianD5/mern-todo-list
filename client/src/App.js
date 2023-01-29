b1c8c6163f1a5365784f2f925fb7407171d48332
LilianD5 <diazliliandev@gmail.com>
2023-01-29 15:47:40 +0100

    CSS Style for tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Welcome, Trou Duc</h1>
+            <h3>Your tasks</h3>
+
+            <div className="tasks">
+				<div className="task">
+					<div className="checkbox"></div>
+
+					<div className="description"><p>Get the bread</p></div>
+
+					<div className="delete-task"><img src="../icons/delete.svg" alt="delete-icon" className="delete-icon" /></div>
+				</div>
+				<div className="task is-complete">
+					<div className="checkbox"></div>
+
+					<div className="description"><p>Get the milk</p></div>
+
+					<div className="delete-task"><img src="../icons/delete.svg" alt="delete-icon" className="delete-icon" /></div>
+				</div>
+			</div>
+        </div>
+    );
 }
 
 export default App;
